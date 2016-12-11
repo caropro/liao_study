37599b74d3400aae92abac186b321b340df39111
caropro <1470130895@qq.com>
2016-12-11 15:12:06 +0800

    Add files via upload

--- the basic_1.py
+++ the basic_1.py
@@ -5,8 +5,8 @@ s2=float(input("今年成绩"))
 
 r=abs((s1-s2)/s1*100)
 if s1>s2:
-    print("成绩下降了 %.1f" % r)
+    print("成绩下降了 %.1f %%" % r)
 elif s1 == s2:
     print("成绩没变化")
 else:
-    print("成绩提升了了 %.1f" % r)
+    print("成绩提升了了 %.1f %%" % r)
